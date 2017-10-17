fcdde5298693a5a01ec8568ca1a0778959223871
Joe Dluzen <jdluzen@gmail.com>
2017-10-16 22:02:00 -0400

    Update preproc

--- CryptSharp/Properties/AssemblyInfo.cs
+++ CryptSharp/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-#if !NETSTANDARD1_6
+#if !NETSTANDARD1_3
 [assembly: AssemblyCompany("Illusory Studios LLC")]
 [assembly: AssemblyCopyright("Copyright 2011, 2013 James F. Bellinger <http://www.zer7.com/software/cryptsharp>")]
 [assembly: AssemblyProduct("CryptSharp")]
